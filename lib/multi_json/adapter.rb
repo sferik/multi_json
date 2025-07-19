b1ee2e856ab547d7c8356cd9425eecff93ce64a9
Erik Berlin <sferik@gmail.com>
2025-07-18 21:01:09 -0700

    Allow blank input for load

--- lib/multi_json/adapter.rb
+++ lib/multi_json/adapter.rb
@@ -26,7 +26,7 @@ module MultiJson
 
       def load(string, options = {})
         string = string.read if string.respond_to?(:read)
-        raise self::ParseError if blank?(string)
+        return nil if blank?(string)
 
         instance.load(string, cached_load_options(options))
       end
