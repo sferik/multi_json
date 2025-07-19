270c1e7ab0fa9dd791ac7e18f300ef065d4b28a1
Erik Berlin <sferik@gmail.com>
2025-07-18 23:35:44 -0700

    Shorten method

--- spec/shared/json_common_adapter.rb
+++ spec/shared/json_common_adapter.rb
@@ -7,12 +7,8 @@ shared_examples_for "JSON-like adapter" do |adapter|
     describe "with :pretty option set to true" do
       it "passes default pretty options" do
         object = "foo"
-        expect(JSON).to receive(:generate).with(object, {
-          indent: "  ",
-          space: " ",
-          object_nl: "\n",
-          array_nl: "\n"
-        }).and_call_original
+        options = {indent: "  ", space: " ", object_nl: "\n", array_nl: "\n"}
+        expect(JSON).to receive(:generate).with(object, options).and_call_original
         MultiJson.dump(object, pretty: true)
       end
     end
