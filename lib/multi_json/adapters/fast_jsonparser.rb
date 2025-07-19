a03f16bcc95c6379d140e359a5884ea6ff76dc6d
Erik Berlin <sferik@gmail.com>
2025-07-18 18:59:43 -0700

    Add fast_jsonparser adapter

---
+++ lib/multi_json/adapters/fast_jsonparser.rb
@@ -0,0 +1,49 @@
+require "fast_jsonparser"
+require "oj"
+require_relative "../adapter"
+
+module MultiJson
+  module Adapters
+    # Use the FastJsonparser library to load and Oj to dump.
+    class FastJsonparser < Adapter
+      defaults :load, symbolize_keys: false
+      defaults :dump, mode: :compat, time_format: :ruby, use_to_json: true
+
+      ParseError = ::FastJsonparser::ParseError
+
+      def load(string, options = {})
+        ::FastJsonparser.parse(string, symbolize_keys: options[:symbolize_keys])
+      end
+
+      OJ_VERSION = ::Oj::VERSION
+      OJ_V2 = OJ_VERSION.start_with?("2.")
+      OJ_V3 = OJ_VERSION.start_with?("3.")
+      private_constant :OJ_VERSION, :OJ_V2, :OJ_V3
+
+      if OJ_V3
+        PRETTY_STATE_PROTOTYPE = {
+          indent: "  ",
+          space: " ",
+          space_before: "",
+          object_nl: "\n",
+          array_nl: "\n",
+          ascii_only: false
+        }.freeze
+        private_constant :PRETTY_STATE_PROTOTYPE
+      end
+
+      def dump(object, options = {})
+        if OJ_V2
+          options[:indent] = 2 if options[:pretty]
+          options[:indent] = options[:indent].to_i if options[:indent]
+        elsif OJ_V3
+          options.merge!(PRETTY_STATE_PROTOTYPE.dup) if options.delete(:pretty)
+        else
+          raise "Unsupported Oj version: #{::Oj::VERSION}"
+        end
+
+        ::Oj.dump(object, options)
+      end
+    end
+  end
+end
