c112a06a782d4639494c929c971c49cc093fa608
Erik Berlin <sferik@gmail.com>
2025-12-01 12:26:32 -0800

    Refactor json_gem spec helper

--- spec/multi_json/adapters/json_gem_spec.rb
+++ spec/multi_json/adapters/json_gem_spec.rb
@@ -27,16 +27,20 @@ RSpec.describe MultiJson::Adapters::JsonGem, :json do
       RUBY
     end
 
-    it "prettifies output when :pretty is true" do
+    def run_script(script_content)
       Tempfile.create(["multi_json_json_gem", ".rb"]) do |file|
-        file.write(script)
+        file.write(script_content)
         file.flush
         file.close
 
-        output, status = Open3.capture2(RbConfig.ruby, file.path)
-
-        expect([status.success?, output]).to eq([true, expected_output])
+        Open3.capture2(RbConfig.ruby, file.path)
       end
+    end
+
+    it "prettifies output when :pretty is true" do
+      output, status = run_script(script)
+
+      expect([status.success?, output]).to eq([true, expected_output])
     end
   end
 end
