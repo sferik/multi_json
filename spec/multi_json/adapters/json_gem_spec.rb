31cd77b24ae5b9d2f5fa8e18e8e0352510c29e04
Erik Berlin <sferik@gmail.com>
2025-12-19 08:15:30 -0800

    Refactor specs for RuboCop compliance

--- spec/multi_json/adapters/json_gem_spec.rb
+++ spec/multi_json/adapters/json_gem_spec.rb
@@ -11,50 +11,26 @@ RSpec.describe MultiJson::Adapters::JsonGem, :json do
   it_behaves_like "JSON-like adapter", described_class
 
   describe ".dump" do
-    before { MultiJson.use described_class }
+    let(:klass) { Class.new { def as_json(*) = {abc: "def"} } }
+    let(:object) { klass.new }
+
+    before do
+      MultiJson.use described_class
+      allow(object).to receive(:as_json).and_call_original
+    end
 
     it "calls as_json on objects that respond to it" do
-      klass = Class.new do
-        def as_json(*)
-          {abc: "def"}
-        end
-      end
-      object = klass.new
-      allow(object).to receive(:as_json).and_call_original
       MultiJson.dump(object)
       expect(object).to have_received(:as_json)
     end
 
     it "calls as_json on objects that respond to it with pretty option" do
-      klass = Class.new do
-        def as_json(*)
-          {abc: "def"}
-        end
-      end
-      object = klass.new
-      allow(object).to receive(:as_json).and_call_original
       MultiJson.dump(object, pretty: true)
       expect(object).to have_received(:as_json)
     end
   end
 
   context "when active_support/json is loaded" do
-    let(:data) { {a: 1, b: 2, c: {d: {f: 2}}} }
-    let(:expected_output) { "#{JSON.pretty_generate(data)}\n" }
-
-    let(:script) do
-      <<~RUBY
-        $LOAD_PATH.unshift File.expand_path("../../lib", __dir__)
-        require "multi_json"
-        require "multi_json/adapters/json_gem"
-        require "active_support/json"
-
-        data = {a: 1, b: 2, c: {d: {f: 2}}}
-
-        puts MultiJson.dump(data, pretty: true, adapter: :json_gem)
-      RUBY
-    end
-
     def run_script(script_content)
       Tempfile.create(["multi_json_json_gem", ".rb"]) do |file|
         file.write(script_content)
@@ -65,45 +41,37 @@ RSpec.describe MultiJson::Adapters::JsonGem, :json do
       end
     end
 
-    it "prettifies output when :pretty is true" do
-      output, status = run_script(script)
-
-      expect([status.success?, output]).to eq([true, expected_output])
-    end
-
-    it "serializes objects that define to_hash" do
-      script = <<~RUBY
+    def activesupport_script(code)
+      <<~RUBY
         $LOAD_PATH.unshift File.expand_path("../../lib", __dir__)
         require "multi_json"
         require "multi_json/adapters/json_gem"
         require "active_support/json"
 
-        class MyClass
-          def to_hash
-            {abc: "def"}
-          end
-        end
-
-        puts MultiJson.dump(MyClass.new, adapter: :json_gem)
+        #{code}
       RUBY
+    end
 
-      output, status = run_script(script)
+    let(:pretty_script) { activesupport_script("puts MultiJson.dump({a: 1, b: 2, c: {d: {f: 2}}}, pretty: true, adapter: :json_gem)") }
+    let(:to_hash_script) do
+      activesupport_script('Class.new { def to_hash = {abc: "def"} }.then { puts MultiJson.dump(_1.new, adapter: :json_gem) }')
+    end
+    let(:time_script) { activesupport_script("puts MultiJson.dump(Time.utc(2025, 12, 4, 13, 39, 46, 705000), adapter: :json_gem)") }
+
+    it "prettifies output when :pretty is true" do
+      output, status = run_script(pretty_script)
+
+      expect([status.success?, output]).to eq([true, "#{JSON.pretty_generate({a: 1, b: 2, c: {d: {f: 2}}})}\n"])
+    end
+
+    it "serializes objects that define to_hash" do
+      output, status = run_script(to_hash_script)
 
       expect([status.success?, output]).to eq([true, "{\"abc\":\"def\"}\n"])
     end
 
-    it "serializes Time objects using ActiveSupport format" do
-      script = <<~RUBY
-        $LOAD_PATH.unshift File.expand_path("../../lib", __dir__)
-        require "multi_json"
-        require "multi_json/adapters/json_gem"
-        require "active_support/json"
-
-        time = Time.utc(2025, 12, 4, 13, 39, 46, 705000)
-        puts MultiJson.dump(time, adapter: :json_gem)
-      RUBY
-
-      output, status = run_script(script)
+    it "serializes Time objects using ActiveSupport format", :aggregate_failures do
+      output, status = run_script(time_script)
 
       expect(status.success?).to be(true)
       expect(output).to include("2025-12-04T13:39:46.705Z")
