c57d126a7e5824f5fbf399ba8f2e2213d1ad54a0
Erik Berlin <sferik@gmail.com>
2025-12-19 17:39:48 -0800

    Simplify json_gem dump optimization

--- lib/multi_json/adapters/json_gem.rb
+++ lib/multi_json/adapters/json_gem.rb
@@ -26,9 +26,8 @@ module MultiJson
 
       def dump(object, options = {})
         opts = options.except(:adapter)
-        return ::JSON.dump(object) if opts.empty? && !object.respond_to?(:as_json)
-
         json_object = object.respond_to?(:as_json) ? object.as_json : object
+        return ::JSON.dump(json_object) if opts.empty?
 
         if opts.delete(:pretty)
           opts = PRETTY_STATE_PROTOTYPE.merge(opts)
