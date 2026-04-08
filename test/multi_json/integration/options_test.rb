66978224ed5cb277f1b5d505f6f7783091d0ae31
Erik Berlin <sferik@gmail.com>
2026-04-08 17:51:22 -0600

    Tighten cover directive on the Options integration test

--- test/multi_json/integration/options_test.rb
+++ test/multi_json/integration/options_test.rb
@@ -2,7 +2,7 @@ require_relative "../../test_helper"
 require_relative "../../support/options_tests"
 
 class OptionsIntegrationTest < Minitest::Test
-  cover "MultiJson*"
+  cover "MultiJson::Options*"
 
   include OptionsTests
 
